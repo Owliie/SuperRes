9b83c3b0ed21e799860b2fba072105d05d69fbb7
Victor Velev <velev.victor@yahoo.com>
2019-10-19 17:12:26 +0300

    [Update] finish dataset pipeline

--- super_resolution/data.py
+++ super_resolution/data.py
@@ -5,10 +5,15 @@ from os.path import basename, exists, join
 
 from torchvision.transforms import transforms
 
-from .CONFIG import *
+from CONFIG import *
+from dataset import DatasetFromFolder
 
 __all__ = [
     'download_bsd300',
+    'input_transform',
+    'target_transform',
+    'get_train_set',
+    'get_test_set',
 ]
 
 
@@ -22,31 +27,46 @@ def download_bsd300(dest='./dataset'):
         print('downloading url...', url)
         data = urllib.request.urlopen(url)
 
-        file_path = join(dest, basename(url))
-        with open(file_path, 'wb') as f:
+        filepath = join(dest, basename(url))
+        with open(filepath, 'wb') as f:
             f.write(data.read())
 
         print('extracting data...')
-        with tarfile.open(file_path) as tar:
+        with tarfile.open(filepath) as tar:
             for item in tar:
                 tar.extract(item, dest)
 
-        remove(file_path)
+        remove(filepath)
 
     return output_image_dir
 
-def input_transform(h=IMAGE_HEIGHT, w=IMAGE_WIDTH, scale_factor=SCALE_FACTOR):
+def input_transform(h, w, scale_factor):
     return transforms.Compose([
         transforms.CenterCrop((h, w)),
         transforms.Resize((h // scale_factor, w // scale_factor)),
         transforms.ToTensor(),
     ])
 
-def target_transforms(h=IMAGE_HEIGHT, w=IMAGE_WIDTH):
+def target_transform(h, w):
     return transforms.Compose([
         transforms.CenterCrop((h, w)),
         transforms.ToTensor(),
     ])
+
+def get_train_set(h=IMAGE_HEIGHT, w=IMAGE_WIDTH, scale_factor=SCALE_FACTOR):
+    return DatasetFromFolder(
+        join(download_bsd300(), 'train'),
+        input_transfrom=input_transform(h, w, scale_factor),
+        target_transform=target_transform(h, w),
+    )
+
+def get_test_set(h=IMAGE_HEIGHT, w=IMAGE_WIDTH, scale_factor=SCALE_FACTOR):
+    return DatasetFromFolder(
+        join(download_bsd300(), 'test'),
+        input_transfrom=input_transform(h, w, scale_factor),
+        target_transform=target_transform(h, w),
+    )
+
 
 
 if __name__ == '__main__':
---
+++ super_resolution/dataset.py
@@ -0,0 +1,43 @@
+from os import listdir
+from os.path import join
+
+import torch.utils.data as data
+from PIL import Image
+
+__all__ = [
+    'is_image_file',
+    'load_image',
+    'DatasetFromFolder',
+]
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for exntension in ['.png', '.jpg', '.jpeg'])
+
+def load_image(filepath):
+    img = Image.open(filepath).convert('YCbCr')
+    Y, _, _ = img.split()
+    return Y
+
+class DatasetFromFolder(data.Dataset):
+    
+    def __init__(self, folderpath, input_transfrom=None, target_transform=None):
+        super(DatasetFromFolder, self).__init__()
+        self.image_filenames = [join(folderpath, x) for x in listdir(folderpath) if is_image_file(x)]
+
+        self.input_transfrom = input_transfrom
+        self.target_transform = target_transform
+
+    def __getitem__(self, idx):
+        input = load_image(self.image_filenames[idx])
+        target = input.copy()
+
+        if self.input_transfrom:
+            input = self.input_transfrom(input)
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        return input, target
+
+    def __len__(self):
+        return len(self.image_filenames)
